613d946b7fd1586071fa82b441dbb52ae90168ee
Jakob Dunning <jakob.dunnning@luckycloud.de>
2022-01-25 23:28:43 +0100

    Removed comments, changed naming, simplified code

--- wordcounter.py
+++ wordcounter.py
@@ -1,29 +1,20 @@
 import re
 import os
-path_of_folder = "somewhere on your computer"
-filepath = f'{path_of_folder}'
-final_dictionary = {}
+input_path = "songs"
+global_dictionary = {}
 
-#----functions----#
-""" The open_file function opens a file via a context manager and returns
-    the content of the file as lowercase characters
-"""
-def open_file(filename):
-    with open(f'{filepath}\{filename}') as f:
+def get_file_contents(path):
+    with open(path) as f:
         content = f.read().lower()
+
     return content
 
-"""
-The unwanted_char_check function receives its data from the open_file function
-and uses regex to split the content into a list while excluding symbols,
-white space characters and other symbols that are not considered to be words.
-"""
-def unwanted_char_check(filecontent):
+def remove_symbols(filecontent):
     pattern = r"\-*\'*\b\w+"
-    clean_content = re.findall(pattern, filecontent)
-    return clean_content
 
-def words_counter(array):
+    return re.findall(pattern, filecontent)
+
+def count_words(array):
     word_dictionary = {}
     for word in array:
         if word != "":
@@ -33,17 +24,17 @@ def words_counter(array):
                 word_dictionary[word] = 1
     return word_dictionary
 
-#----actual processing----#
 
-for filename in os.listdir(filepath):
-    content_of_file = open_file(filename)
-    cleaned_up_content = unwanted_char_check(content_of_file)
-    result = words_counter(cleaned_up_content)
+for filename in os.listdir(input_path):
+    content = get_file_contents(os.path.join(input_path, filename))
+    content = remove_symbols(content)
+    dictionary = count_words(content)
 
-    for name in result.keys():
-        if name in final_dictionary.keys():
-            final_dictionary[name] += result[name]
-        else:
-            final_dictionary[name] = result[name]
+    for name in dictionary.keys():
+        if name not in global_dictionary.keys():
+            global_dictionary[name] = 0
 
-print({k: v for k, v in sorted(final_dictionary.items(), key=lambda item: item[1])})
+        global_dictionary[name] += dictionary[name]
+
+for item in sorted(global_dictionary.items()):
+    print(item)
