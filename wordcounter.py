389e1e744bc89939f10642145ca00b0201b7cdf2
Jakob Dunning <jakob.dunnning@luckycloud.de>
2022-01-25 23:35:46 +0100

    Add blanklines from PEP8, renamed function

--- wordcounter.py
+++ wordcounter.py
@@ -1,7 +1,9 @@
 import re
 import os
+
 input_path = "songs"
 global_dictionary = {}
+
 
 def get_file_contents(path):
     with open(path) as f:
@@ -9,32 +11,22 @@ def get_file_contents(path):
 
     return content
 
-def remove_symbols(filecontent):
+
+def filter_out_words(filecontent):
     pattern = r"\-*\'*\b\w+"
 
     return re.findall(pattern, filecontent)
 
-def count_words(array):
-    word_dictionary = {}
-    for word in array:
-        if word != "":
-            if word in word_dictionary:
-                word_dictionary[word] += 1
-            else:
-                word_dictionary[word] = 1
-    return word_dictionary
-
 
 for filename in os.listdir(input_path):
     content = get_file_contents(os.path.join(input_path, filename))
-    content = remove_symbols(content)
-    dictionary = count_words(content)
+    word_list = filter_out_words(content)
 
-    for name in dictionary.keys():
-        if name not in global_dictionary.keys():
-            global_dictionary[name] = 0
+    for word in word_list:
+        if word not in global_dictionary.keys():
+            global_dictionary[word] = 0
 
-        global_dictionary[name] += dictionary[name]
+        global_dictionary[word] += 1
 
 for item in sorted(global_dictionary.items()):
     print(item)
